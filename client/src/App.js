d77f503c6912c645973be7aa39ce5c957d98e113
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-03-11 00:05:59 -0800

    Added routing to ClientForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import Navigation from "./components/Navigation";
-import Home from './Home';
-import Login from './Login';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ClientForm from './pages/ClientForm';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <Navigation/>
       <Switch>
-        <Route path="/" exact component={Home}/>
         <Route path="/login" exact component={Login}/>
+        <Route path="/clientform" exact component={ClientForm}/>
+        <Route path="/" exact component={Home}/>
       </Switch>
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
     </Router>
   );
 }
