9440677ee886fd924e77ebfb0bb3ce7a37ee1515
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-04-06 13:17:15 -0700

    Added redirect to Registration page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,43 @@
 import React, { useRef, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import './Login.scss';
 
 export default function Login () {
+  const [redirect, setRedirect] = useState(false)
+  const [error, setError] = useState ( {
+    email: '',
+    password: '',
+  })
   const [user, setUser] = useState(
     { email: '',
       password: ''
     })
-  const email = useRef(null);
-  const password = useRef(null);
+  
+  const validate = () => {
+    if (user.email === "") {
+      setError({...error, email: "Email cannot be blank."});
+      return;
+    }
+    if (user.password === "") {
+      setError({...error, password: "You must provide a password."});
+      return;
+    }
+    if (!user.email.includes("@north-find.com")) {
+      setError({...error, email: "Please enter a valid email"});
+      return;
+    }
+    setError({...error, email:''})
+  }
 
   const changeHandler = (e) => {
     setUser({...user, [e.target.name]: e.target.value})
   }
   const handleSubmit = (e) => {
     e.preventDefault();
+    validate();
+    // Axios request to db to get the User's information and set session cookie.
   }
   console.log(user)
 
@@ -26,20 +47,23 @@ export default function Login () {
         <h1>Sign In</h1>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" as="input" autoComplete="off" placeholder="Enter email" ref={email} onChange = {changeHandler}/>
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control type="email" name="email" as="input" autoComplete="off" placeholder="Enter email" onChange = {changeHandler}/>
+          <section className="register-validation">{error.email}</section>
+          {/* <Form.Text className="text-muted">
+            
+          </Form.Text> */}
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" as="input" autoComplete="off" placeholder="Password" ref={password} onChange = {changeHandler}/>
+          <Form.Control type="password" name="password" as="input" autoComplete="off" placeholder="Password" onChange = {changeHandler}/>
+          <section className="register-validation">{error.password}</section>
         </Form.Group>
         <Button variant="primary" type="submit" onClick={handleSubmit}>
           Submit
         </Button>
       </Form>
+      <Link to={"/register"}>Don't have an account? Register Here</Link>
     </div>
   )
 }
