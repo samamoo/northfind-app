4dde1e5a99b8ab802ab917f83525e0804f9a6ae0
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-05-06 17:50:20 -0700

    Added user details to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './Profile.scss';
 
 export default function Profile(props) {
-  console.log(props.state)
-  const [cookie, setCookie] = useState({})
-
-  useEffect(() => {
-    axios.get("http://localhost:9000/api/login/")
-    .then(res => console.log(res, "People"))
-  },[]);
+  const user = props.state.userData
 
   return(
     <div>
-      {cookie && 
       <main className="profilepage">
-        <h1>PROFILE PAGE</h1>
-        <p>hello there</p>
+        <h1>Your Profile</h1>
+        <p>Name: {user.first_name} {user.last_name}</p>
+        <p>Email: {user.email}</p>
+
       </main>
-      }
     </div>
   )
 }
