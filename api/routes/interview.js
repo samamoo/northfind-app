cc9768e7a2e4252e9584050496b14050046a05fe
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-05-11 14:38:16 -0700

    Added routes to interview

---
+++ api/routes/interview.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+  // Get all interview sessions
+  router.get('/', (req, res) => {
+    db.query(`SELECT * FROM interviews;`)
+    .then((data) => {
+      if (data) {
+        res.status(200).send(data.rows[0]);
+      } else {
+        res.send(false);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  });
+
+  // Create a new interview session
+  router.post('/', (req, res) => {
+    
+  })
+}
