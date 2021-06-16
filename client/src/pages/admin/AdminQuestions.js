14bc2d5ff6d513207fe882dc9a3822d29472a584
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-06-15 22:57:16 -0700

    Created page for viewing and adding new questions

---
+++ client/src/pages/admin/AdminQuestions.js
@@ -0,0 +1,94 @@
+import React , { useState, useEffect } from 'react';
+import { Modal, Button, Form, FormControl } from 'react-bootstrap'
+import axios from 'axios';
+import './AdminQuestions.scss';
+
+export default function AdminQuestions() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [areas, setAreas] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const [questionList, setQuestionList] = useState( {
+    questions: []
+  });
+
+  // Get list of interview questions
+  useEffect(() => {
+    axios.get('http://localhost:9000/api/questions/')
+    .then(res => {
+      setQuestionList((prev) => ({...prev, questions: res.data}))
+    })
+  },[]);
+  useEffect(() => {
+    axios.get('http://localhost:9000/api/areas/')
+    .then(res => {
+      setAreas(res.data)
+    })
+  },[]);
+  useEffect(() => {
+    axios.get('http://localhost:9000/api/groups/')
+    .then(res => {
+      setGroups(res.data)
+    })
+  },[]);
+  console.log(groups)
+
+  // Open and Close modal
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return(
+    <main className="admin-client">
+      <div className="admin-client-banner">
+        <h1>Interview Questions</h1>
+        <button className="btn btn-primary" onClick={openModal}>+ New Question</button>
+      </div>
+      <div className="admin-client-searchbar-content">
+        <p>Search:</p>
+        <span>
+          <input 
+          className="textfield" 
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="Search by Area or Group"
+          autoComplete='off' 
+          onChange={event => {setSearchTerm(event.target.value)}}>
+          </input> 
+        </span>
+      </div>
+      <div className="line"></div>
+      <div className="admin-questions-searchresults">
+        <table>
+          <thead>
+            <tr>
+              <th>Group</th>
+              <th>Area</th>
+              <th>Question</th>
+              <th>Weight Factor</th>
+              <th>Assessment Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questionList.questions.map((val) => {
+              for (const area of areas) {
+                for (const group of groups) {
+                  if (val.area_id === area.id && area.group_id === group.id) {
+                    return (
+                      <tr key={val.id}>
+                        <td>{group.name}</td>
+                        <td>{area.name}</td>
+                        <td>{val.notes}</td>
+                        <td>{val.weight}</td>
+                        <td>{val.assessment}</td>
+                      </tr>
+                    )
+                  }
+                }
+              }
+            })}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
+}
