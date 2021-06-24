3d895d928249142c5f12f26ce2787c570cc6e6a2
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-06-24 09:09:45 -0700

    Added validation

--- client/src/pages/admin/AddQuestionModal.js
+++ client/src/pages/admin/AddQuestionModal.js
@@ -16,7 +16,59 @@ export default function AddQuestionModal(props) {
     group: '',
   });
 
+  const validate = () => {
+    if (question.notes === '') {
+      setError({...error, notes: 'You must set a question.'});
+      return false;
+    };
+    if (question.weight === 0 || question.weight === "Select...") {
+      setError({...error, weight: 'You must set a weigth factor.'});
+      return false;
+    };
+    if (question.assessment === 0 || question.assessment === "Select...") {
+      setError({...error, assessment: 'You must set an assessment value.'});
+      return false;
+    };
+    if (question.area === '' || question.area === "Select...") {
+      setError({...error, area: 'You must assign an area.'});
+      return false;
+    };
+    if (question.group === '' || question.group === "Select...") {
+      setError({...error, group: 'You must assign a group.'});
+      return false;
+    };
+    setError({...error, notes:'', weight:'', assessment: '', group: '', area:''});
+    return true;
+  }
 
+  const selection = (e) => {
+    let value = 0;
+    if (e.target.value === "1") {
+      value = 1;
+    } else if (e.target.value === "2") {
+      value = 2;
+    } else if (e.target.value === "3") {
+      value = 3;
+    } else if (e.target.value === "4") {
+      value = 4;
+    } else if (e.target.value === "5") {
+      value = 5;
+    } 
+    setQuestion({...question, [e.target.name]: value})
+  };
+
+  const changeHandler = (e) => {
+    setQuestion({...question, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    console.log(question)
+    props.closeModal();
+  }
 
   return(
     <Modal show={props.modalIsOpen} onHide={props.closeModal}>
@@ -26,43 +78,50 @@ export default function AddQuestionModal(props) {
       <Form className="form-container" style={{width: "100%"}}>
         <Modal.Body>
           <Form.Label>Question</Form.Label>
-          <Form.Control type="text" name="notes" as="textarea" autoComplete="off" rows={3}></Form.Control>
+          <Form.Control type="text" name="notes" as="textarea" autoComplete="off" rows={3} onChange={changeHandler}></Form.Control>
           <section className="register-validation">{error.notes}</section>
           <Form.Row>
             <Form.Group as={Col}>
               <Form.Label>Weight Factor</Form.Label>
-              <Form.Control name="weight" as="select">
+              <Form.Control name="weight" as="select" onChange={selection}>
+                <option>Select...</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
                 <option>4</option>
                 <option>5</option>
               </Form.Control>
+              <section className="register-validation">{error.weight}</section>
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Assessment Value</Form.Label>
-              <Form.Control name="assessment" as="select">
+              <Form.Control name="assessment" as="select" onChange={selection}>
+                <option>Select...</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
                 <option>4</option>
                 <option>5</option>
               </Form.Control>
+              <section className="register-validation">{error.assessment}</section>
             </Form.Group>
           </Form.Row>
           <Form.Group>
             <Form.Label>Group</Form.Label>
-            <Form.Control as="select" name="group">
+            <Form.Control as="select" name="group" onChange={changeHandler}>
+              <option>Select...</option>
               <option>Performance Measurement</option>
               <option>Process</option>
               <option>Organization</option>
               <option>Tools</option>
             </Form.Control>
+            <section className="register-validation">{error.group}</section>
           </Form.Group>
           <Form.Group>
             {/* Filter the areas available depending on the group selected */}
             <Form.Label>Area</Form.Label>
-            <Form.Control as="select" name="area">
+            <Form.Control as="select" name="area" onChange={changeHandler}>
+              <option>Select...</option>
               <option>OTTR</option>
               <option>Forecast Accuracy</option>
               <option>MPS Attainment</option>
@@ -82,11 +141,12 @@ export default function AddQuestionModal(props) {
               <option>Systems Control</option>
               <option>Systems Functionality</option>
             </Form.Control>
+            <section className="register-validation">{error.area}</section>
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={props.closeModal}>Cancel</Button>
-          <Button variant="primary">Save Changes</Button>
+          <Button variant="primary" onClick={handleSubmit}>Save Changes</Button>
         </Modal.Footer>
       </Form>
     </Modal>
