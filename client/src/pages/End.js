9fbce94b10bc7ed37a589e8b7bd13ca650ce381d
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-06-30 11:48:31 -0700

    Created page for when the interview ends

---
+++ client/src/pages/End.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './End.scss';
+
+export default function End() {
+  return(
+    <main className="end">
+      <h1>Interview Session Complete.</h1>
+      <a href="/">Back to home</a>
+    </main>
+  )
+}
