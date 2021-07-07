2f9bfae351d55b3bfb6bcbba5b4ef9fd69cee0d1
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-07-07 00:25:21 -0700

    Created template for Interview session page

---
+++ client/src/pages/Interview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Interview.scss';
+
+export default function Interview(props) {
+  return(
+    <main className="interview">
+      <h1>Interview Session in Progress</h1>
+    </main>
+  )
+}
