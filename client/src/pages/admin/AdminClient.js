4e6876bff97af27a849e83bb9aa47e977e237968
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-07-08 23:22:45 -0700

    Added a delete modal and logic for deleting a company name

--- client/src/pages/admin/AdminClient.js
+++ client/src/pages/admin/AdminClient.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Button, Form, FormControl } from 'react-bootstrap'
-// import Modal from 'react-modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import './AdminClient.scss';
 import AddClientModal from './AddClientModal';
+import DeleteClientModal from './DeleteClientModal';
 import BackToTop from '../../components/BackToTop';
 import DownToBottom from '../../components/DownToBottom';
 
 export default function AdminClient () {
   const [searchTerm, setSearchTerm] = useState("");
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState({
+    open: false,
+    id: null,
+  });
   const [companyList, setCompanyList] = useState( {
     companies: []
   });
@@ -22,9 +28,14 @@ export default function AdminClient () {
       setCompanyList((prev) => ({...prev, companies: res.data}))
     })
   },[]);
+
   // Open and Close modal
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
+  // Open and Close Delete modal
+  const openDeleteModal = (id) => {setDeleteModalIsOpen((prev) => ({...prev, open: true, id}))};
+  const closeDeleteModal = () => setDeleteModalIsOpen(false);
+
   // Filter the search results
   const filterSearch = () => {
     let results = [];
@@ -49,7 +60,17 @@ export default function AdminClient () {
     .catch((err) => {
       console.log(err);
     })
-  }
+  };
+  const deleteCompany = (id) => {
+    axios.post("http://localhost:9000/api/company/delete", {id} )
+    .then ((res) => {
+      const newList = res.data;
+      setCompanyList((prev) => ({...prev, companies: newList}))
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
 
   return(
     <main className="admin-client">
@@ -72,13 +93,34 @@ export default function AdminClient () {
         </span>
       </div>
       <div className="line"></div>
-      <div className="admin-client-searchresults">
-        {filterSearch().map((val,key) => {
-          return(
-            <p key={val.id}>{val.company_name}</p>
-          )
-        })}
+      <div className="admin-questions-searchresults">
+        <table>
+          <thead>
+            <tr>
+              <th className="table-heading">Name</th>
+              <th className="table-heading">Edit</th>
+              <th className="table-heading">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterSearch().map((val,key) => {
+              return(
+                <tr>
+                  <td key={val.id}>{val.company_name}</td>
+                  <td style={{'width': '10%'}}><FontAwesomeIcon id="edit-icon" style={{'color': '#037ffc'}} icon={faEdit}/></td>
+                  <td style={{'width': '10%'}}><FontAwesomeIcon id="trash-icon" style={{'color': '#dc3545'}} icon={faTrashAlt} onClick={() => openDeleteModal(val.id)}/></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
+        {deleteModalIsOpen &&
+          <DeleteClientModal 
+          closeDeleteModal={closeDeleteModal}
+          deleteModalIsOpen={deleteModalIsOpen}
+          deleteCompany={deleteCompany}/>
+        }
         {modalIsOpen && 
           <AddClientModal 
           closeModal={closeModal} 
