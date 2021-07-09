e88a5225e56798c4247a92f6a87c048c36f893f5
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-07-08 23:20:43 -0700

    Added ordering for select all and added delete route for deleting a client

--- api/routes/company.js
+++ api/routes/company.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res, next) => {
-    db.query(`SELECT * FROM companies;`)
+    db.query(`SELECT * FROM companies ORDER BY company_name;`)
     .then((data) => {
       if (data) {
         res.status(200).send(data.rows);
@@ -21,7 +21,7 @@ module.exports = (db) => {
     console.log(req.body.companyName)
     db.query(`INSERT INTO companies (company_name) VALUES ($1);`,[company])
     .then(() => {
-      db.query(`SELECT * FROM companies;`)
+      db.query(`SELECT * FROM companies ORDER BY company_name;`)
       .then((x) => {
         const list = x.rows;
         res.status(200).send(list)
@@ -31,5 +31,21 @@ module.exports = (db) => {
       res.status(500).json({ error: err.message });
     });
   });
+  // Delete a company
+  router.post('/delete', (req, res) => {
+    const id = req.body.id;
+    console.log(id)
+    db.query(`Delete from companies where id = ($1) RETURNING *;`, [id])
+    .then(() => {
+      db.query(`SELECT * FROM companies ORDER BY company_name;`)
+      .then((data) => {
+        const list = data.rows;
+        res.status(200).send(list);
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  })
   return router;
 }
