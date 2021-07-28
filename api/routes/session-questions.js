7f7b678215883f602214c6f1ba7cfc9928ad51c8
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-07-28 11:53:51 -0700

    Created queries for the session-questions for dashboard

--- api/routes/session-questions.js
+++ api/routes/session-questions.js
@@ -2,31 +2,90 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-
-
-  // Create a new interview session
-  router.post('/', (req, res) => {
-    console.log(req.body);
-   const arrayObject = req.body.sessionQuestions.questions;
-   const session_id = req.body.sessionId;
-   for( let val of arrayObject ) {
-     const question_id = val.id;
-     const comments = val.comments;
-     const assessment = val.assessment;
-     const score = val.score;
-     const arrayParams = [question_id, session_id, comments, assessment, score];
-     db.query(
-      `INSERT INTO session_questions (question_id, session_id, comments, assessment, score) VALUES($1, $2, $3, $4, $5) RETURNING *`, arrayParams
-    )
+  router.get('/', (req, res) => {
+    let perfMeas = [];
+    let process = [];
+    let organization = [];
+    let tools = [];
+    // Performance Measurement Group
+    db.query(`SELECT * FROM session_questions
+    JOIN questions 
+    ON question_id = questions.id 
+    WHERE questions.area_id = 1
+    OR questions.area_id = 2
+    OR questions.area_id = 3
+    OR questions.area_id = 4
+    OR questions.area_id = 5
+    OR questions.area_id = 6
+    OR questions.area_id = 7
+    ORDER BY questions.area_id;`)
     .then((data) => {
-      res.status(200).json(data.rows[0])
+      perfMeas = data.rows;
+      db.query(`SELECT * FROM session_questions
+      JOIN questions 
+      ON question_id = questions.id 
+      WHERE questions.area_id = 8
+      OR questions.area_id = 9
+      OR questions.area_id = 10
+      OR questions.area_id = 11
+      OR questions.area_id = 12
+      OR questions.area_id = 13
+      ORDER BY questions.area_id;`)
+      .then((b) => {
+        process = b.rows;
+        db.query(`SELECT * FROM session_questions
+        JOIN questions 
+        ON question_id = questions.id 
+        WHERE questions.area_id = 14
+        OR questions.area_id = 15
+        OR questions.area_id = 16
+        ORDER BY questions.area_id;`)
+        .then((c) => {
+          console.log(c.rows);
+          organization = c.rows;
+          db.query(`SELECT * FROM session_questions
+          JOIN questions 
+          ON question_id = questions.id 
+          WHERE questions.area_id = 17
+          OR questions.area_id = 18
+          ORDER BY questions.area_id;`)
+          .then((d) => {
+            console.log(d.rows)
+            tools = d.rows;
+            res.status(200).send({perfMeas, process, organization, tools})
+          })
+        })
+      })
     })
     .catch((err) => {
       res.status(500).json({error: err.message});
     })
-   }
-    // Create a new interview session
-    
+  })
+
+  // Create a new interview session
+  router.post('/', (req, res) => {
+    const arrayObject = req.body.sessionQuestions.questions;
+    const length = arrayObject.length;
+    const session_id = req.body.sessionId;
+    for( let val of arrayObject ) {
+      const question_id = val.id;
+      const comments = val.comments;
+      const assessment = val.assessment;
+      const score = val.score;
+      const arrayParams = [question_id, session_id,comments, assessment, score];
+      db.query(
+        `INSERT INTO session_questions (question_id, session_id, comments, assessment, score) VALUES($1, $2, $3, $4, $5) RETURNING *`, arrayParams
+      )
+    }
+    db.query(
+        `SELECT * FROM session_questions WHERE session_id = ${session_id}`
+      )
+      .then((data) => {
+        res.status(200).json(data.rows)
+      })
+      .catch((err) => {
+        res.status(500).json({error: err.message});
+    })
   })
   return router;
 }
