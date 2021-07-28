1b9ab1316d6649b10e0f6bc6ee113e2c84dde4e6
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-07-28 11:56:18 -0700

    Working on admin dashboard to display scores data

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function AdminDashboard(props) {
-  const [groupScore, setGroupScore] = useState({
+  const [groups, setGroups] = useState({
     process:'',
     performanceMeasurement:'',
+    organization:'',
+    tools:'',
+  });
+  const [groupScore, setGroupScore] = useState({
+    process:'',
+    performanceMeasurement:0,
     organization:'',
     tools:'',
   });
@@ -26,9 +32,46 @@ export default function AdminDashboard(props) {
     systemsFunctionality:'',
   })
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:9000/api/session-questions/")
-  // },[])
+  useEffect(() => {
+    axios.get("http://localhost:9000/api/session-questions/")
+    .then((res) => {
+      setGroups((prev) => ({...prev, performanceMeasurement: res.data.perfMeas, process: res.data.process, organization: res.data.organization, tools: res.data.tools}))
+      getScores();
+    })
+  },[]);
+
+  const weightedAverage = (arr) => {
+    // Loop through an array of a group
+    // Add all of the items' scores
+    // Add all of the items' weight
+    // Divide scores sum by weight sum to get weighted avg of that group.
+    let weightedAvg = 0;
+    let sumOfWeights = 0;
+    let sumOfScores = 0;
+    if (arr === []) {
+      return 0;
+    }
+    for (const item of arr) {
+      sumOfScores = sumOfScores + item.score;
+      sumOfWeights = sumOfWeights + item.weight;
+    }
+    weightedAvg = sumOfScores / sumOfWeights;
+    parseFloat(weightedAvg).toFixed(2)
+    return weightedAvg;
+  }
+  
+  const getScores = () => {
+    const pmScore = weightedAverage(groups.performanceMeasurement)
+    console.log(pmScore)
+    // const processScore = weightedAverage(groups.process)
+    // const orgScore = weightedAverage(groups.organization)
+    // const toolsScore = weightedAverage(groups.tools)
+    // setGroupScore((prev) => ({...prev, performanceMeasurement: pmScore}));
+    setGroupScore({...groupScore, performanceMeasurement: pmScore})
+  }
+
+  console.log(groups)
+  console.log(groupScore)
 
   return(
     <main className="clientform">
